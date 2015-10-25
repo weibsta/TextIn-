c7a17a0de3f7a333c85bc533106527d674fefaa0
Weibin <weibin.zhong1994@gmail.com>
2015-10-25 10:07:54 -0700

    AND HIS NAME IS JOHN CENA

--- send_sms.py
+++ send_sms.py
@@ -1,15 +1,12 @@
 from twilio.rest import TwilioRestClient
 
-account = "ACee602631b49abf6c3c6d759237683b5b"
-token = "e5b9d682049ceeec4fb19478bd594051"
+account = ""
+token = ""
 client = TwilioRestClient(account, token)
 
 
-#message = client.messages.create(to="+14088384091", from_="+14087249293", 
-#				 body="Hello there!")
-
-call = client.calls.create(to="4082212915",
-			  from_="14087249293",
+call = client.calls.create(to="",
+			  from_="",
 			  url="http://www.myinstants.com/media/sounds/and-his-name-is-john-cena-1.mp3")
 
 print(call.sid)
